98d593805155f9f2688c8946aacfe7c69a13f63d
Mary Delos Reyes <mdelosreyes3@ucmerced.edu>
2020-02-01 14:57:16 -0800

    home page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'Pages/LogIn.dart';
-import 'Pages/Camera.dart';
-import 'Pages/Email.dart';
+// import 'Pages/Camera.dart';
+// import 'Pages/Email.dart';
 import 'Pages/Home.dart';
-import 'Pages/Search.dart';
+// import 'Pages/Search.dart';
 
 void main() {
   runApp(
@@ -14,17 +14,8 @@ void main() {
           case '/':
             return MaterialPageRoute(builder: (context) => LogIn());
             break;
-          case '/home':
-            return MaterialPageRoute(builder: (context) => Home());
-            break;
           case '/email':
-            return MaterialPageRoute(builder: (context) => Email());
-            break;
-          case '/camera':
-            return MaterialPageRoute(builder: (context) => Camera());
-            break;
-          case '/search':
-            return MaterialPageRoute(builder: (context) => Search());
+            return MaterialPageRoute(builder: (context) => Home());
             break;
         }
       },
