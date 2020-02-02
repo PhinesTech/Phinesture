e913795d58e9ed0006d99931cdbd18d73eb07213
Adrian Darian <adarian@ucmerced.edu>
2020-02-02 07:51:28 -0800

    current status

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:math';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -20,6 +21,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context).settings.arguments;
+    final username = args.toString().split(",")[0].substring(1);
 
     PageController controller = PageController(initialPage: images.length - 1);
     controller.addListener(() {
@@ -29,141 +31,143 @@ class _HomeState extends State<Home> {
     });
 
     return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [
-            Color(0xFF4CAF50),
-            Color(0xFF6078ea),
-          ],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              tileMode: TileMode.clamp)),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(
-                        Icons.navigate_before,
-                        color: Colors.white,
-                        size: 30.0,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.account_circle,
-                        color: Colors.white,
-                        size: 30.0,
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text("Hello, ${args.toString().split(",")[0].substring(1)}",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 44.0,
-                          fontFamily: "Calibre-Semibold",
-                          letterSpacing: 1.0,
-                        )),
-                  ],
-                ),
-              ),
-              Stack(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+              Color(0xFF4CAF50),
+              Color(0xFF6078ea),
+            ],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                tileMode: TileMode.clamp)),
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: SingleChildScrollView(
+              child: Column(
                 children: <Widget>[
-                  CardScrollWidget(currentPage),
-                  Positioned.fill(
-                    child: PageView.builder(
-                      itemCount: images.length,
-                      controller: controller,
-                      reverse: true,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          margin: const EdgeInsets.only(top: 420.0, left: 15.0),
-                          alignment: Alignment.center,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              InkWell(
-                                child: Container(
-                                  width: ScreenUtil.getInstance().setWidth(590),
-                                  height:
-                                      ScreenUtil.getInstance().setHeight(50),
-                                  decoration: BoxDecoration(
-                                      gradient: LinearGradient(colors: [
-                                        Color(0xFF17ead9),
-                                        Color(0xFF6078ea)
-                                      ]),
-                                      borderRadius: BorderRadius.circular(40.0),
-                                      boxShadow: [
-                                        BoxShadow(
-                                            color: Color(0xFF6078ea)
-                                                .withOpacity(.3),
-                                            offset: Offset(0.0, 8.0),
-                                            blurRadius: 8.0)
-                                      ]),
-                                  child: Material(
-                                    color: Colors.transparent,
-                                    child: InkWell(
-                                      child: Center(
-                                        child: Text("Name of Plant",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontFamily: "Poppins-Bold",
-                                                fontSize: 18,
-                                                letterSpacing: 1)),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(
+                            Icons.navigate_before,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.account_circle,
+                            color: Colors.white,
+                            size: 30.0,
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Hello, ${username}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 44.0,
+                              fontFamily: "Calibre-Semibold",
+                              letterSpacing: 1.0,
+                            )),
+                      ],
+                    ),
+                  ),
+                  Stack(
+                    children: <Widget>[
+                      CardScrollWidget(currentPage),
+                      Positioned.fill(
+                        child: PageView.builder(
+                          itemCount: images.length,
+                          controller: controller,
+                          reverse: true,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              margin:
+                                  const EdgeInsets.only(top: 420.0, left: 15.0),
+                              alignment: Alignment.center,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  InkWell(
+                                    child: Container(
+                                      width: ScreenUtil.getInstance()
+                                          .setWidth(590),
+                                      height: ScreenUtil.getInstance()
+                                          .setHeight(50),
+                                      decoration: BoxDecoration(
+                                          gradient: LinearGradient(colors: [
+                                            Color(0xFF17ead9),
+                                            Color(0xFF6078ea)
+                                          ]),
+                                          borderRadius:
+                                              BorderRadius.circular(40.0),
+                                          boxShadow: [
+                                            BoxShadow(
+                                                color: Color(0xFF6078ea)
+                                                    .withOpacity(.3),
+                                                offset: Offset(0.0, 8.0),
+                                                blurRadius: 8.0)
+                                          ]),
+                                      child: Material(
+                                        color: Colors.transparent,
+                                        child: InkWell(
+                                          child: Center(
+                                            child: Text("Name of Plant",
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontFamily: "Poppins-Bold",
+                                                    fontSize: 18,
+                                                    letterSpacing: 1)),
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
+                                ],
                               ),
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  )
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Container(
+                        alignment: Alignment.bottomCenter,
+                        margin: const EdgeInsets.only(
+                          top: 100.0,
+                        ),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: const Text('Gallery',
+                              style: TextStyle(
+                                fontSize: 30,
+                              )),
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          elevation: 10,
+                        ),
+                      ))
                 ],
               ),
-              Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Container(
-                    alignment: Alignment.bottomCenter,
-                    margin: const EdgeInsets.only(
-                      top: 100.0,
-                    ),
-                    child: RaisedButton(
-                      onPressed: () {},
-                      child: const Text('Gallery',
-                          style: TextStyle(
-                            fontSize: 30,
-                          )),
-                      color: Colors.blue,
-                      textColor: Colors.white,
-                      elevation: 10,
-                    ),
-                  ))
-            ],
-          ),
-        ),
-      ),
-    );
+            )));
   }
 }
 
@@ -178,19 +182,11 @@ class CardScrollWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return new AspectRatio(
       aspectRatio: widgetAspectRatio,
-      child: LayoutBuilder(builder: (context, contraints) {
-        var width = contraints.maxWidth;
-        var height = contraints.maxHeight;
-        // print('width: ' + width.toString());
-        // print('height: ' + height.toString());
-
-        var safeWidth = width - 2 * padding;
-        var safeHeight = height - 2 * padding;
-
-        var heightOfPrimaryCard = safeHeight;
-        var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
-
-        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+      child: LayoutBuilder(builder: (context, constraints) {
+        var primaryCardLeft = constraints.maxWidth -
+            2 * padding -
+            constraints.maxHeight -
+            2 * padding * cardAspectRatio;
         var horizontalInset = primaryCardLeft / 2;
 
         List<Widget> cardList = new List();
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'Pages/LogIn.dart';
 import 'Pages/Home.dart';
+import 'Pages/Plant.dart';
 import 'Pages/Profile.dart';
+import 'Pages/MessageList.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'Phinesture',
+    options: const FirebaseOptions(
+      googleAppID: "1:685688366527:android:05a526c0720170829ced0a",
+      gcmSenderID: "685688366527",
+      apiKey: "AIzaSyC0TANY6rcyQC6USsL92huM4OENhqKmmWM",
+      projectID: "phinesture",
+    ),
+  );
+  final Firestore firestore = Firestore(app: app);
+
   runApp(
     MaterialApp(
       title: 'Phinesture',
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => LogIn(),
+        // '/home': (BuildContext context) => MessageList(firestore: firestore),
         '/home': (BuildContext context) => Home(),
-        '/profile': (BuildContext context) => Profile(),
+        // '/profile': (BuildContext context) => Profile(),
+        '/plant': (BuildContext context) => Plant(),
       },
       debugShowCheckedModeBanner: false,
     ),
