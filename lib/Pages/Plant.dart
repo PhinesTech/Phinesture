188c7bfecd094664aa0b231cfb193e7b800f8a29
Athena Siaotong <asiaotong@ucmerced.edu>
2020-02-02 00:29:44 -0800

    Set up plant page with background and scaffold

--- lib/Pages/Plant.dart
+++ lib/Pages/Plant.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../Widgets/LoginCard.dart';
 
 class Plant extends StatefulWidget {
   @override
@@ -11,23 +9,51 @@ class _PlantState extends State<Plant> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/window_background.png"),
+    // return Container(
+    //   decoration: BoxDecoration(
+    //     image: DecorationImage(
+    //       image: AssetImage("assets/window_background.png"),
+    //       fit: BoxFit.cover,
+    //     ),
+    //   ),
+    //   child: new Scaffold(
+    //     body: Center(
+    //       child: Text('Scaffold child'), // plant image
+    //     ),
+    //   ),
+    // );
+    return Stack(
+      children: <Widget>[
+
+        // Background image (window)
+        Image.asset(
+          'assets/window_background.png',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
           fit: BoxFit.cover,
         ),
-      ),
-      child: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 32,
-            color: Colors.black,
+
+        // Scaffold for layout
+        Scaffold(
+          backgroundColor: Colors.transparent, 
+          
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            title: Text('Sample Title'), //Insert upper icons
           ),
+
+          body: Center(
+            child: Image.asset('assets/stage9.png'), //Insert plant image
+          ),
+
+          bottomNavigationBar: BottomAppBar(
+            child: Text('hi2'), //Insert swipe up to see stats
+          ),
+
         ),
-      ),
+
+      ],
     );
   }
 }
